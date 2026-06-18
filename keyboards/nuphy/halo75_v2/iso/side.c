f441bac2a4a3bb3329ae73df47ba682d390e2ad6
Ellie Augustini <ellie@augustini.wtf>
2026-06-19 00:14:23 +0200

    Slow down startup animation to compensate for fewer rim LEDs
    - Changed from side_speed_table[0] to side_speed_table[1] for slower timing
    - Startup animation now lights 39 rim LEDs (5-43) instead of 45
    - Slower timing should make the animation look smoother

--- keyboards/nuphy/halo75_v2/iso/side.c
+++ keyboards/nuphy/halo75_v2/iso/side.c
@@ -447,10 +447,11 @@ uint8_t is_side_rgb_on(uint8_t index)
 
 static void side_power_mode_show(void)
 {
-    if (side_play_cnt <= side_speed_table[0][side_speed])
+    // Use slower speed since we have fewer rim LEDs (39 instead of 45)
+    if (side_play_cnt <= side_speed_table[1][side_speed])
         return;
     else
-        side_play_cnt -= side_speed_table[0][side_speed];
+        side_play_cnt -= side_speed_table[1][side_speed];
     if (side_play_cnt > 20) side_play_cnt = 0;
 
     // Start from index 5 to skip status LEDs (0-4)
